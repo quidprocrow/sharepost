60bc200bd452c32e3f9c690cbf57d3612ab012d6
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 16:46:15 -0400

    Autoload libraries
    Autoload libraries in bootstrap file.

--- phpmvc/app/bootstrap.php
+++ phpmvc/app/bootstrap.php
@@ -3,10 +3,19 @@
 // Load configuration file
 require_once 'config/config.php';
 
-// Load Libraries
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+// // Load Libraries
+// require_once 'libraries/Core.php';
+// require_once 'libraries/Controller.php';
+// require_once 'libraries/Database.php';
+
+// Autoload Core Libraries.
+// This function will work, as long as file's name is the same
+// as the class it contains.
+// This is useful if the list of libraries gets very long.
+spl_autoload_register(function($className){
+  require_once 'libraries/' . $className . '.php';
+
+});
 
 
 ?>
