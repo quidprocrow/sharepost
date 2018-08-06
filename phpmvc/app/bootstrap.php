7a0c2dd50e5a6151c81432887a3b0fbbae53ec03
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 17:43:11 -0400

    Include header and footer in about view
    Include header and footer in about view.

--- phpmvc/app/bootstrap.php
+++ phpmvc/app/bootstrap.php
@@ -11,6 +11,8 @@ require_once 'config/config.php';
 // Autoload Core Libraries.
 // This function will work, as long as file's name is the same
 // as the class it contains.
+// Hence why the file names are capitalized to match the uppercase
+// syntax expected of classes.
 // This is useful if the list of libraries gets very long.
 spl_autoload_register(function($className){
   require_once 'libraries/' . $className . '.php';
