703608d9c7c3ed3c2b4072d493484886e94416de
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 15:32:27 -0400

    Add method and parameter functionality
    Check to see if the URL has a second part.
    If so, check to see if there is a method within the controller that
    corresponds to that second part.
    If not, the default is the index method.
    
    Make any remaining parts of the url into the params variable, or make
    params an empty array if not.
    
    Use call_user_func_array to call the currentMethod on the currentController
    (defaulting to the index method on the Pages controller if unspecified)
    with any specified params.
    
    I understand this last part the least.

--- phpmvc/app/libraries/core.php
+++ phpmvc/app/libraries/core.php
@@ -35,6 +35,27 @@
 
       // Instantiate controller.
       $this->currentController = new $this->currentController;
+
+      // Check for second part of url.
+      if(isset($url[1])){
+        // Check to see that the second part of the url corresponds to an existing method.
+        if(method_exists($this->currentController, $url[1])){
+          // If it does exist, make it the current method.
+          // If it doesn't, the default is set to index.
+          $this->currentMethod = $url[1];
+
+          // After it becomes the current method, unset it from url.
+          unset($url[1]);
+        }
+      }
+
+      // Grab all url parameters.
+      // If url is set / truthy, make params an array consisting of
+      // the values of the url array; if it isn't, make it an empty array.
+      $this->params = $url ? array_values($url) : [];
+
+      // Call a callback with an array of params.
+      call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl(){
