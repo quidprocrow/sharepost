4b59dd13c3f6962d617acd448b7ff0065041770f
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 15:36:43 -0400

    Add an about and index method to the Pages controller
    Add an index method to the pages controller, as it is the default.
    
    Add an about method that takes parameters, to test the callback functionality.

--- phpmvc/app/controllers/Pages.php
+++ phpmvc/app/controllers/Pages.php
@@ -4,6 +4,14 @@
     public function __construct(){
       echo 'Pages loaded';
     }
+
+    public function index(){
+      echo 'You found index!';
+    }
+
+    public function about($id){
+      echo 'This is about, with id' . $id;
+    }
   }
 
 
