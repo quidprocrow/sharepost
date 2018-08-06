dce7b0efe6286364dbd413c413ce4d1a1061af00
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 15:52:13 -0400

    Create base Controller class
    Create base controller class with a model and view function.
    
    Model function requires the model file and instantiates it.
    
    View function checks for an applicable view.
    
    If it does exist, requires it.
    
    If it does not, gives an error stating as much.

--- phpmvc/app/libraries/controller.php
+++ phpmvc/app/libraries/controller.php
@@ -1,1 +1,33 @@
+<?php
+
+/* Base Controller
+Loads models and views
+*/
+
+class Controller {
+  // Load model
+  public function model($model){
+
+    // Require model file.
+    require_once '../app/models/' . $model . '.php';
+
+    // Instantiate the model.
+    return new $model();
+  }
+
+  public function view($view, $data = []){
+    // Check to see that such a vew file exists.
+    if(file_exists('../app/views/' . $view . '.php')){
+      // If it does exist, require it.
+      require_once '../app/views/' . $view . '.php';
+    } else {
+      // If the file does not exist, show an error.
+      die('View does not exist!');
+    }
+  }
+}
+
+?>
+
+
 Controller!
