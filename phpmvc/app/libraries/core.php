6b959a5bb699e5c13ebccc9fc0b1e5ad09ec28a8
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 14:45:11 -0400

    Modify getUrl method and construct method
    Modify getUrl method to trim the trailing slash after the parameters,
    sanitize the url, and create an array based on the remaining slashes,
    but only when there is such a parameter specified.
    
    (No more errors when nothing is specified!)
    
    When constructed, check to see if there is a controller file that
    corresponds to the first part of the url.
    
    If there is, make that currentController (instead of Pages).
    
    Require the file corresponding to currentController. EG, Pages, if
    there is no file that corresponds to the url parameter.
    
    Make current controller a new instance of that controller.

--- phpmvc/app/libraries/core.php
+++ phpmvc/app/libraries/core.php
@@ -11,13 +11,44 @@
     protected $params = [];
 
     public function __construct(){
-      $this->getUrl();
+      // Confirm getUrl working by printing the array.
+      // print_r($this->getUrl());
 
+      $url = $this->getUrl();
+
+      // Look in controllers for the first value.
+      // Checks the controller folder for a php file corresponding to
+      // the first parameter.
+      // Using ucwords to ensure the file is uppercase, as is standard.
+      if(file_exists('../app/controllers/' . ucwords($url[0]) . 'php')){
+
+        // If there is such a controller, make it the current one.
+        $this->$currentController =  ucwords($url[0]);
+        // After it becomes the new current controller, remove it from the url array.
+        unset($url[0]);
+      }
+
+      // Require the controller.
+      // This will be the pages controller or the controller specified through
+      // url parameters.
+      require_once '../app/controllers/' .$this->currentController . 'php';
+
+      // Instantiate controller.
+      $this->$currentController = new $this->$currentController;
     }
 
     public function getUrl(){
-      echo $_GET['url'];
-
+      // First, check to see that there is a url parameter.
+      if(isset($_GET['url'])){
+        // Strip out the trailing slash.
+        $url = rtrim($_GET['url'], '/');
+        // Filter out any naughty bits.
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        // Create an array based on the remaining slashes.
+        $url = explode('/', $url);
+        // Return the url.
+        return $url;
+      }
     }
   }
 
