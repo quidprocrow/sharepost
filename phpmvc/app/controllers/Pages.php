6c0a1d60273ad8b0d6ce32c1f7741568aae1bc11
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 18:50:30 -0400

    Remove Post references
    Remove post references.
    
    Tutorial prompted to remove database information; I left that unchanged
    as I will likely need it.

--- phpmvc/app/controllers/Pages.php
+++ phpmvc/app/controllers/Pages.php
@@ -2,14 +2,11 @@
 
   class Pages extends Controller {
     public function __construct(){
-      $this->postModel = $this->model('Post');
 
     }
 
     public function index(){
-      $posts = $this->postModel->getPosts();
       $data = [
-        'posts' => $posts,
         'title' => 'Welcome'
       ];
       $this->view('pages/index', $data);
--- phpmvc/app/views/pages/index.php
+++ phpmvc/app/views/pages/index.php
@@ -2,10 +2,4 @@
 
 <h1><?php echo $data['title']; ?></h1>
 
-<ul>
-    <?php foreach($data['posts'] as $post) : ?>
-      <li><?php echo $post->title; ?></li>
-    <?php endforeach; ?>
-</ul>
-
 <?php require APPROOT . '/views/inc/footer.php'; ?>
