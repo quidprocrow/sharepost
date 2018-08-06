8b5a3dbadf3cef2db1aaa0fc32ad116858894c24
quidprocrow <windmillwarrior@gmail.com>
2018-08-06 18:37:51 -0400

    Add methods to Database class
    Add methods to Database class to prepare SQL statement, bind values to it,
    execute that statement, and fetch results as either single objects or
    an array of objects, or the number of rows.

--- phpmvc/app/libraries/database.php
+++ phpmvc/app/libraries/database.php
@@ -35,6 +35,58 @@ class Database {
       echo $this->error;
     }
   }
+
+  public function query($sql){
+    // Prepares a sql statement.
+    $this->stmt = $this->dbh->prepare($sql);
+  }
+
+  // Binds values to that sql statement.
+  public function bind($param, $value, $type = null){
+    // Test to see the value type.
+    // If nothing is set as the type ...
+    if(is_null($type)){
+      // Check to see what its type is using php functions.
+      // Assign it accordingly.
+      switch(true){
+        case is_int($value):
+          $type = PDO::PARAM_INT;
+          break;
+        case is_bool($value):
+          $type = PDO::PARAM_BOOL;
+          break;
+        case is_null($value):
+          $type = PDO::PARAM_NULL;
+          break;
+        default:
+          $type = PDO::PARAM_STR;
+      }
+    }
+    // Bind the values.
+    $this->stmt->bindValue($param, $value, $type);
+  }
+
+  public function execute(){
+    return $this->stmt->execute();
+  }
+
+  // Get result set as array of objects.
+  public function resultSet(){
+    $this->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+  }
+
+  // Get a single result as an object.
+  public function single(){
+    $this->execute();
+    return $this->stmt->fetch(PDO::FETCH_OBJ);
+  }
+
+  public function rowCount(){
+    // Row count is a PDO method. 
+    return $this->stmt->rowCount();
+  }
+
 }
 
 ?>
